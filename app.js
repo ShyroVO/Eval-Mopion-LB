11602fd9be60ee228b7ce2fd224cf41b3ca3ee44
ShyroVO <Bosquet.laurie@outlook.com>
2020-12-09 14:32:12 +0100

    Joueur gagnant alert // Test ajout de point et reset des cases.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // Anti clic droit:
-/*
 document.addEventListener('contextmenu', function (event){
     event.preventDefault();
 });
- */
+
 // Selection joueur X ou O qui commence:
 let compteur = Math.floor(Math.random()* 2 + 1);
 let quiJoue = document.getElementById('quiJoue');
+let buttonColor = document.getElementById('game');
 
 if (compteur === 1) {
     quiJoue.innerHTML = "Joueur X qui commence!";
@@ -32,11 +32,13 @@ function aQuiLeTour () {
 function elementsJoueurX () {
     aQuiLeTour();
     compteur = 2;
+    buttonColor.classList.add("colorPlayerX");
 }
 
 function elementsJoueurO () {
     aQuiLeTour();
     compteur = 1;
+    buttonColor.classList.add("colorPlayerO");
 }
 
 // Boucle action sur les cases (bouton):
@@ -55,11 +57,13 @@ for (let caseMorpion of casesMorpion) {
                     document.getElementById('case1').innerHTML= pionX;
                     elementsJoueurX()
                     one = true;
+                    checkCase();
                 }
                 else {
                     document.getElementById('case1').innerHTML= pionO;
                     elementsJoueurO()
                     one = false;
+                    checkCase();
                 }
                 break;
 
@@ -68,11 +72,13 @@ for (let caseMorpion of casesMorpion) {
                     document.getElementById('case2').innerHTML= pionX;
                     elementsJoueurX()
                     two = true;
+                    checkCase();
                 }
                 else {
                     document.getElementById('case2').innerHTML= pionO;
                     elementsJoueurO()
                     two = false;
+                    checkCase();
                 }
                 break;
 
@@ -81,11 +87,13 @@ for (let caseMorpion of casesMorpion) {
                     document.getElementById('case3').innerHTML= pionX;
                     elementsJoueurX()
                     three = true;
+                    checkCase();
                 }
                 else {
                     document.getElementById('case3').innerHTML= pionO;
                     elementsJoueurO()
                     three = false;
+                    checkCase();
                 }
                 break;
 
@@ -94,11 +102,13 @@ for (let caseMorpion of casesMorpion) {
                     document.getElementById('case4').innerHTML= pionX;
                     elementsJoueurX()
                     four = true;
+                    checkCase();
                 }
                 else {
                     document.getElementById('case4').innerHTML= pionO;
                     elementsJoueurO()
                     four = false;
+                    checkCase();
                 }
                 break;
 
@@ -107,11 +117,13 @@ for (let caseMorpion of casesMorpion) {
                     document.getElementById('case5').innerHTML= pionX;
                     elementsJoueurX()
                     five = true;
+                    checkCase();
                 }
                 else {
                     document.getElementById('case5').innerHTML= pionO;
                     elementsJoueurO()
                     five = false;
+                    checkCase();
                 }
                 break;
 
@@ -120,11 +132,13 @@ for (let caseMorpion of casesMorpion) {
                     document.getElementById('case6').innerHTML= pionX;
                     elementsJoueurX()
                     six = true;
+                    checkCase();
                 }
                 else {
                     document.getElementById('case6').innerHTML= pionO;
                     elementsJoueurO()
                     six = false;
+                    checkCase();
                 }
                 break;
 
@@ -133,11 +147,13 @@ for (let caseMorpion of casesMorpion) {
                     document.getElementById('case7').innerHTML= pionX;
                     elementsJoueurX()
                     seven = true;
+                    checkCase();
                 }
                 else {
                     document.getElementById('case7').innerHTML= pionO;
                     elementsJoueurO()
                     seven = false;
+                    checkCase();
                 }
                 break;
 
@@ -146,11 +162,13 @@ for (let caseMorpion of casesMorpion) {
                     document.getElementById('case8').innerHTML= pionX;
                     elementsJoueurX()
                     eight = true;
+                    checkCase();
                 }
                 else {
                     document.getElementById('case8').innerHTML= pionO;
                     elementsJoueurO()
                     eight = false;
+                    checkCase();
                 }
                 break;
 
@@ -159,11 +177,13 @@ for (let caseMorpion of casesMorpion) {
                     document.getElementById('case9').innerHTML= pionX;
                     elementsJoueurX()
                     nine = true;
+                    checkCase();
                 }
                 else {
                     document.getElementById('case9').innerHTML= pionO;
                     elementsJoueurO()
                     nine = false;
+                    checkCase();
                 }
                 break;
 
@@ -173,4 +193,34 @@ for (let caseMorpion of casesMorpion) {
 
         }
     })
+}
+
+// Victoire de qui:
+function checkCase() {
+    let playerX = document.getElementById("playerX");
+    let playerO = document.getElementById('playerO');
+    let playerXpoint = 1;
+    let playerOpoint = 1;
+
+    if (one === true && two === true && three === true || four === true && five === true && six === true ||
+        seven === true && eight === true && nine === true || one === true && four === true && seven === true ||
+        two === true && five === true && eight === true || three === true && six === true && nine === true ||
+        one === true && five === true && nine === true || three === true && five === true && seven === true) {
+
+        playerX.innerHTML= playerXpoint + "pts";
+        playerXpoint++;
+        alert("Joueur X Gagne!");
+        document.getElementsByTagName('button').innerHTML = "";
+    }
+
+    else if (one === false && two === false && three === false || four === false && five === false && six === false ||
+        seven === false && eight === false && nine === false || one === false && four === false && seven === false ||
+        two === false && five === false && eight === false || three === false && six === false && nine === false ||
+        one === false && five === false && nine === false || three === false && five === false && seven === false) {
+
+        playerO.innerHTML= playerOpoint + "pts";
+        playerOpoint++;
+        alert("Joueur O Gagne!");
+        document.getElementsByTagName('button').innerHTML = "";
+    }
 }
